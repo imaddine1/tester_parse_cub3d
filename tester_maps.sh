2e751bffbb82b294c584a1dd9c800f42188203be
imaddine1 <hh.imad2015@gmail.com>
2022-11-09 11:48:36 +0100

    change the display

--- tester_maps.sh
+++ tester_maps.sh
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    tester_maps.sh                                     :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: iharile <iharile@student.42.fr>            +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2022/11/08 21:45:37 by iharile           #+#    #+#              #
-#    Updated: 2022/11/08 22:11:40 by iharile          ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 #!bin/bash
 EXEC='cub3D'
 PATH1="maps_test/invalid/"
@@ -22,21 +10,21 @@ END="\033[0;m"
 
 echo -e "${BLUE}CHECK INVALID MAPS${END}"
 for f in $PATH1/* ; do
-	check=$(./cub3D  $f | wc -l)
-	if [ $check -ge 1 ]
-	then
-		echo -e "${GREEN}Good ---> $f ${END}"
-	else
-		echo -e "${RED}false ---> $f ${END}"
-	fi
+    check=$(./cub3D  $f | wc -l)
+    if [ $check -ge 1 ]
+    then
+        echo -e "$f......${GREEN}OK${END}"
+    else
+        echo -e "$f......${RED}KO${END}"
+    fi
 done
 echo -e "${BLUE}CHECK VALID MAPS${END}"
 for f in $PATH2/* ; do
-	check=$(./cub3D  $f | wc -l)
-	if [ $check -lt 1 ]
-	then
-		echo -e "${GREEN}Good ---> $f ${END}"
-	else
-		echo -e "${RED}false ---> $f ${END}"
-	fi
+    check=$(./cub3D  $f | wc -l)
+    if [ $check -lt 1 ]
+    then
+        echo -e "$f......${GREEN}OK${END}"
+    else
+        echo -e "$f......${RED}KO${END}"
+    fi
 done
